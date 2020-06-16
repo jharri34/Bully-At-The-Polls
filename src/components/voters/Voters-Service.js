9734de2f3f1b30a0c2ecfa2e38a5737a41a36447
vagrant <bobbyprather@yahoo.com>
2020-06-15 20:25:38 -0500

    Broken loop looking into

--- src/components/voters/Voters-Service.js
+++ src/components/voters/Voters-Service.js
@@ -1,16 +1,15 @@
 // eslint-disable-next-line
-import config from '../shared/config'
+import config from '../shared/config';
 
 export async function getVoters(address) {
-  let url =config.ELECTION_URL + "?key=" +config.API_KEY + "&address="+address
-    console.log(`voters url .... ${url}`);
+	let url = config.ELECTION_URL + config.QUERY_KEY + config.API_KEY + config.ADDRESS_KEY + address;
+	console.log(`voters url .... ${url}`);
 
-    let data =await fetch(url)
-    try {
-      const resp = await data.json()
-      return resp  
-    }catch(exception){
-      return exception
-    }
-    
-  }   
+	let data = await fetch(url);
+	try {
+		const resp = await data.json();
+		return resp;
+	} catch (exception) {
+		return exception;
+	}
+}
