61c6b6b1c5306f158e9be8c10e14385519fff18b
Harris, Jamall <Jamall.Harris.ctr@gtri.gatech.edu>
2020-06-23 15:32:02 -0400

    added space between nav links

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -16,12 +16,12 @@ function Header() {
 			<Navbar bg="light" expand="lg">
 				<Navbar.Brand href="#home">BULLY AT THE POLLS</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#link"> About Us</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="/about">About</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Navbar>
 		</div>
 	);
