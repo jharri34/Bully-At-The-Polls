c6effb7830dda34cc02a0d140ce285056a076f48
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-23 12:41:27 -0400

    styled about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,10 +11,10 @@ function About() {
 	return (
         <div class='about'>
            
-            <div>About Us</div>
-            <div>This is a tool for looking up who your elected representatives are, upcoming elections, and histrically infomation about these representatives across the United States. Based on your address, we can find all the federal, state, county and local officials, elections, polling location,officals who represent you in government. <br />
-			Knowing who these representatives are and contacting their offices directly is the most effective way to change how our government work. <br />
-			Have an issue you care about? Find your rep and tell them!</div>
+            <div className='about-title'>About Us</div>
+            <div className='about-text'>This is a tool for looking up who your elected representatives are, upcoming elections, and histrically infomation about these representatives across the United States. Based on your address, we can find all the federal, state, county and local officials, elections, polling location,officals who represent you in government. <br /> <br />
+			Knowing who these representatives are and contacting their offices directly is the most effective way to change how our government work. <br /> <br />
+			Have an issue you care about? <br /> Find your rep and tell them!</div>
 			
 			
 		</div>
