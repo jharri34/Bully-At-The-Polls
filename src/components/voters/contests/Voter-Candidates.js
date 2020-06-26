bae76cfdebfd7d9bfcb8415c054b29e4f8f10ac0
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-25 23:18:28 -0400

    mapping through candidate channels to print info, bug-map undefined

--- src/components/voters/contests/Voter-Candidates.js
+++ src/components/voters/contests/Voter-Candidates.js
@@ -10,6 +10,13 @@ function VoterCandidates({ candidate }) {
 					<div>{ candid.party }</div>
 					<div>{ candid.candidateUrl }</div>
 					<div>{ candid.phone }</div>
+					{/* map through channels array and print out social medial handle(type) and url(id)) */ }
+					<div>{ candid.channels.map((channel, item) =>
+						<div>
+							<div>{ channel.type }</div>
+							<div>{ channel.id }</div>
+						</div>
+					) }</div>
 				</div>
 			)) }
 
