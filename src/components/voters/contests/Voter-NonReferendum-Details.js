37626613e8df08ecca18025b5d9e5132cdd2c54f
vagrant <bobbyprather@yahoo.com>
2020-07-01 19:16:19 -0500

    Added accordion to contest and referendum

--- src/components/voters/contests/Voter-NonReferendum-Details.js
+++ src/components/voters/contests/Voter-NonReferendum-Details.js
@@ -1,18 +1,78 @@
 import React from 'react';
 import VoterCandidates from './Voter-Candidates';
+import { withStyles } from '@material-ui/core/styles';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+
+const Accordion = withStyles({
+	root: {
+		border: '1px solid rgba(0, 0, 0, .125)',
+		boxShadow: 'none',
+		'&:not(:last-child)': {
+			borderBottom: 0
+		},
+		'&:before': {
+			display: 'none'
+		},
+		'&$expanded': {
+			margin: 'auto'
+		}
+	},
+	expanded: {}
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+	root: {
+		backgroundColor: 'rgba(0, 0, 0, .03)',
+		borderBottom: '1px solid rgba(0, 0, 0, .125)',
+		marginBottom: -1,
+		minHeight: 56,
+		'&$expanded': {
+			minHeight: 56
+		}
+	},
+	content: {
+		'&$expanded': {
+			margin: '12px 0'
+		}
+	},
+	expanded: {}
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+	root: {
+		padding: theme.spacing(2)
+	}
+}))(MuiAccordionDetails);
 
 function VoterNonReferendumDetails({ contest }) {
-	console.log(contest)
+	const [ expanded, setExpanded ] = React.useState('panel1');
+
+	const handleChange = (panel) => (event, newExpanded) => {
+		setExpanded(newExpanded ? panel : false);
+	};
+
 	return (
-		<div className='nonreferendum-container'>
-			<div className='nonreferendum-items'>
-				<div className='contest-office'>{ contest.office }</div>
-				<div className='contest-district-items'>
-					<div className='contest-name'>{ contest.district.name }</div>
-					<div className='contest-scope'>{ contest.district.scope }</div>
-				</div>
-			</div>
-			{ <VoterCandidates candidate={ contest } /> }
+		<div className="nonreferendum-container">
+			<Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+				<AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+					<Typography>
+						{' '}
+						<div className="nonreferendum-items">
+							<div className="contest-office">{contest.office}</div>
+							<div className="contest-district-items">
+								<div className="contest-name">{contest.district.name}</div>
+								<div className="contest-scope">{contest.district.scope}</div>
+							</div>
+						</div>
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>{<VoterCandidates candidate={contest} />}</Typography>
+				</AccordionDetails>
+			</Accordion>
 		</div>
 	);
 }
--- src/components/voters/contests/Voter-Referendum-Details.js
+++ src/components/voters/contests/Voter-Referendum-Details.js
@@ -1,15 +1,71 @@
 import React from 'react';
-function VoterReferendumDetails({ contest }) {
-	return (
-		<div className='referendum-container'>
-			<div className='type'>{ contest.type }</div>
-			<div className='title'>{ contest.referendumTitle }</div>
-			<div className='url'>{ contest.referendumUrl }</div>
-			{/* <div>
-				<div>{contest.district.name}</div>
-				<div>{contest.district.scope}</div>
-			</div> */}
+import { withStyles } from '@material-ui/core/styles';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+const Accordion = withStyles({
+	root: {
+		border: '1px solid rgba(0, 0, 0, .125)',
+		boxShadow: 'none',
+		'&:not(:last-child)': {
+			borderBottom: 0
+		},
+		'&:before': {
+			display: 'none'
+		},
+		'&$expanded': {
+			margin: 'auto'
+		}
+	},
+	expanded: {}
+})(MuiAccordion);
 
+const AccordionSummary = withStyles({
+	root: {
+		backgroundColor: 'rgba(0, 0, 0, .03)',
+		borderBottom: '1px solid rgba(0, 0, 0, .125)',
+		marginBottom: -1,
+		minHeight: 56,
+		'&$expanded': {
+			minHeight: 56
+		}
+	},
+	content: {
+		'&$expanded': {
+			margin: '12px 0'
+		}
+	},
+	expanded: {}
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+	root: {
+		padding: theme.spacing(2)
+	}
+}))(MuiAccordionDetails);
+
+function VoterReferendumDetails({ contest }) {
+	const [ expanded, setExpanded ] = React.useState('panel1');
+
+	const handleChange = (panel) => (event, newExpanded) => {
+		setExpanded(newExpanded ? panel : false);
+	};
+
+	return (
+		<div className="referendum-container">
+			<Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+				<AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+					<Typography>
+						<div className="title">{contest.referendumTitle}</div>
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						<div className="url">{contest.referendumUrl}</div>
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
 		</div>
 	);
 }
