ed0bde9fad049fc870f187095e9fe8b440f05ff2
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-15 13:45:51 -0400

    updated format

--- src/components/shared/routes.js
+++ src/components/shared/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  Switch,Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 // import Election from '../components/elections/Elections'
 // import Voters from '../components/voters/Voters'
 import Home from '../home/home'
@@ -8,12 +8,12 @@ import About from '../about/About'
 import Contact from './contact'
 
 const Routes = () => (
-  
+
   <Switch>
-   <Route exact path="/" component={Home}/>
+    <Route exact path="/" component={ Home } />
     <Route exact path="/main" component={ Main } />
     <Route exact path="/about" component={ About } />
-    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/contact" component={ Contact } />
   </Switch>
 );
 
