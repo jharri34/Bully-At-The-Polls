6d1d8af5ab902142e8fc357f5d64ff060d5fa5ea
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-15 19:06:16 -0400

    added form

--- src/components/shared/contact.js
+++ src/components/shared/contact.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
+import {FormControl, Form} from 'react-bootstrap'
 
- function Contact() {
+function Contact() {
     let history = useHistory();
 
     const handleOnClick = () => {
-		history.push('/main');
-	};
+        history.push('/main');
+    };
 
     return (
         <div>
-            
-        </div>
+            <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlInput2">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="name@email.com" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Enter Text</Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                </Form.Group>
+         </Form>
+        </div >
     )
 }
 export default Contact;
