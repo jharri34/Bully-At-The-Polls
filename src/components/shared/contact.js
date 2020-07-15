2f42baabe01f5c5362872fbe527007e83da70426
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-15 13:41:47 -0400

    added contact component. updated footer component

---
+++ src/components/shared/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom';
+
+
+export default function contact() {
+    let history = useHistory();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
