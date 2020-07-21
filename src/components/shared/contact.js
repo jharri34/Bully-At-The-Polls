531af9b9af0a3349ebb3a9df4609d64490b85558
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-21 19:48:19 -0400

    contact form works

--- src/components/shared/contact.js
+++ src/components/shared/contact.js
@@ -3,9 +3,8 @@ import { useHistory, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form'
 import { Form, Col, Row, Button } from 'react-bootstrap'
 import emailjs from "emailjs-com"
+import config from './config'
 
-
-// const [ address, setAddress ] = useState('');
 function Contact() {
 
     const [firstName, setFirstName] = useState('')
@@ -14,51 +13,41 @@ function Contact() {
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
 
-    const { register, handleSubmit, reset } = useForm();
+    const { register, reset } = useForm();
 
-    const onSubmit = (data, e) => {
-        e.preventDefault()
-        console.log(data, e);
-        setFirstName(e.target.value)
-        setLastName(e.target.value)
-        setEmail(e.target.value)
-        setSubject(e.target.value)
-        setMessage(e.target.value)
+    const handleSubmit = (evt) => {
+        evt.preventDefault() 
+        console.log('am i being clicked');
 
-    //     let templateParams = {
-    //         from_name: email,
-    //         to_name: 'bullyatthepolls@gmail.com',
-    //         subject: subject,
-    //         message_html: message,
-    //        }
-    //        emailjs.send(
-    //         'gmail',
-    //         'template_E8o5hEfv',
-    //          templateParams,
-    //         'user_x4DCXAHqhldniEI0cQ2Lj'
-    //        )
-    //        this.resetForm()
-    //    }
-    //   resetForm() {
-    //       this.setState({
-    //         name: '',
-    //         email: '',
-    //         subject: '',
-    //         message: '',
-    //       })
-    //     }
-  
+        setFirstName(evt.target.value)
+        setLastName(evt.target.value)
+        setEmail(evt.target.value)
+        setSubject(evt.target.value)
+        setMessage(evt.target.value)
+
+        let templateParams = {
+            from_name: email,
+            to_name: 'bullyatthepolls@gmail.com',
+            subject: subject,
+            message_html: message,
+        }
+
+        emailjs.send(
+            'gmail',
+            'template_E8o5hEfv',
+            templateParams,
+            'user_x4DCXAHqhldniEI0cQ2Lj'
+        )
+
+        evt.target.reset()
     }
 
-// need to configure emailjs for serverless action
-// need to complet handleSubmit function for form
-// need to remove controlled input in form
 
     let history = useHistory();
 
-    const handleOnClick = () => {
-        history.push('/main');
-    };
+    // const handleOnClick = () => {
+    //     history.push('/main');
+    // };
 
     return (
 
@@ -68,8 +57,8 @@ function Contact() {
             <p>We would love to hear your thoughts, concerns or problems with anything so we can improve</p>
 
             <div class="">
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group controlId="exampleForm.ControlInput1">
+                <Form onSubmit={ handleSubmit }>
+                    <Form.Group controlId="formBasicName">
                         <Form.Row>
                             <Col>
                                 <Form.Control
@@ -77,9 +66,7 @@ function Contact() {
                                     name="firstName"
                                     value={ firstName }
                                     onChange={ e => setFirstName(e.target.value) }
-                                    placeholder="First Name"
-
-                                  />
+                                    placeholder="First Name" />
                             </Col>
                             <Col>
                                 <Form.Control
@@ -102,7 +89,7 @@ function Contact() {
                             placeholder="name@email.com" />
                     </Form.Group>
 
-                    <Form.Group controlId="exampleForm.ControlInput2">
+                    <Form.Group controlId="formBasicSubject">
                         <Form.Label>Subject</Form.Label>
                         <Form.Control
                             type="text"
@@ -111,7 +98,7 @@ function Contact() {
                             onChange={ e => setSubject(e.target.value) } />
                     </Form.Group>
 
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Group controlId="formBasicMessage">
                         <Form.Label>Enter Text</Form.Label>
                         <Form.Control
                             as="textarea"
@@ -121,12 +108,10 @@ function Contact() {
                             onChange={ e => setMessage(e.target.value) } />
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                </Button>
+                    <Button variant="primary" type="submit" >Submit</Button>
                 </Form>
             </div>
-            <a href="mailto:bullyatthepolls@gmail.com" target="_blank">Email</a>
+            {/* <a href="mailto:bullyatthepolls@gmail.com" target="_blank">Email</a> */}
         </div >
     )
 }
