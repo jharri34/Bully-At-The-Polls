6b52aeaa369781b35e81afcc5ab2f812a46e153e
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-21 20:25:34 -0400

    cleaned up code

--- src/components/shared/contact.js
+++ src/components/shared/contact.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory} from 'react-router-dom';
 import { useForm } from 'react-hook-form'
-import { Form, Col, Row, Button } from 'react-bootstrap'
+import { Form, Col, Button } from 'react-bootstrap'
 import emailjs from "emailjs-com"
-import config from './config'
 
 function Contact() {
 
@@ -42,12 +41,6 @@ function Contact() {
         evt.target.reset()
     }
 
-
-    let history = useHistory();
-
-    // const handleOnClick = () => {
-    //     history.push('/main');
-    // };
 
     return (
 
